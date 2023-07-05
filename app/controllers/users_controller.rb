fd155993e287b57b7b0dae39ccd4a5c32377457b
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-07-05 09:40:42 +0800

    Add rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,33 @@ class UsersController < ApplicationController
   before_action :authorized, only: %i[auto_login]
 
   def create
-      @user = User.create(user_params)
-      if @user.valid?
-          token = encode_token({user_id: @user.id})
-          render json: {user: @user, token: token}
-      else
-          render json: {error: "Invalid username or password"}
-      end
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token:}
+    else
+      render json: {error: "Invalid username or password"}
+    end
   end
 
   def login
-      @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username])
 
-      if @user && @user.authenticate(params[:password])
-          token = encode_token({user_id: @user.id})
-          render json: {user: @user, token: token}
-      else
-          render json: {error: "Invalid username or password"}
-      end
+    if @user&.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token:}
+    else
+      render json: {error: "Invalid username or password"}
+    end
   end
 
   def auto_login
-      render json: @user
+    render json: @user
   end
 
   private
 
   def user_params
-      params.permit(:username, :password)
+    params.permit(:username, :password)
   end
 end
