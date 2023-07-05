7d53f078290afa3ad38bd98e2c0f7ae34a66a0f4
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-07-05 13:16:57 +0800

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :characters, dependent: :destroy
+
+  validates :username, presence: true
+  validates :password_digest, presence: true
 end
